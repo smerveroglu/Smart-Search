531d1581d107137c8a147c56a56cdd4416c35926
smerveroglu <sevdimerve@gmail.com>
2022-01-12 23:08:41 +0300

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "bootstrap/dist/css/bootstrap.css";
 
+library.add(fas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
