e994e9a92ea3e0fb3c07c1736a644075b55a0e5e
smerveroglu <sevdimerve@gmail.com>
2022-01-13 02:41:52 +0300

    add table, fix style

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,48 @@
 <template>
-  <div class="list">
-    <p>Phone <font-awesome-icon icon="phone" rotation="90" /></p>
+  <!-- <div class="list"> -->
+    <!-- <p>Phone <font-awesome-icon icon="phone" rotation="90" /></p>
     <p>Email <font-awesome-icon icon="at" /></p>
-    <p>File <font-awesome-icon icon="file-alt" /></p>
+    <p>File <font-awesome-icon icon="file-alt" /></p> -->
+    <!-- <br><br><br> -->
+<div class="table-container">
+<div class="flex-table row">
+  <div class="flex-row rowspan">Ann</div>
+  <div class="column">
+    <div class="flex-row">
+      <div class="flex-cell" ><font-awesome-icon icon="phone" rotation="90"/><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+      <div class="flex-cell" ><font-awesome-icon icon="at" /><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+      <div class="flex-cell" ><font-awesome-icon icon="file-alt" /><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+    </div>
   </div>
+</div>
+<div class="flex-table row">
+  <div class="flex-row rowspan">Ann</div>
+  <div class="column">
+    <div class="flex-row">
+      <div class="flex-cell" ><font-awesome-icon icon="phone" rotation="90"/><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+      <div class="flex-cell" ><font-awesome-icon icon="at" /><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+      <div class="flex-cell" ><font-awesome-icon icon="file-alt" /><span class="ms-2">Napa and Sonoma Wine Country Tour</span></div>
+    </div>
+  </div>
+</div>
+</div>
+    <!-- <section class="table table-striped">
+  <header >
+    <div class="col">Column A</div>
+    <div class="col">Column B</div>
+    <div class="col">Column C</div>
+  </header>
+  <div class="row">
+    <div class="col">1</div>
+    <div class="col">2</div>
+    <div class="col">3</div>
+  </div>
+  <div class="row">
+    <div class="col">1</div>
+    <div class="col">2</div>
+    <div class="col">3</div>
+  </div>
+</section> -->
+  <!-- </div> -->
 </template>
+<style src="../styles/list.scss" lang="scss"></style>
