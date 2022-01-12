9b646f05bb8add52a4d6192ca787ffcfbf9348ca
smerveroglu <sevdimerve@gmail.com>
2022-01-13 00:23:18 +0300

    add sass

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="searchBar">
-    <nav class="navbar sticky-top">
-          <span class="m-4"
-            ><font-awesome-icon icon="search" id="searchIcon" />
-            <input placeholder="Search..." />
-          </span>
-          <!-- <input
-            type="text"
-            class="form-control"
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          /> -->
-
-    </nav>
+    <header class="navbar sticky-top">
+      <nav class="w-100 ms-5">
+        <font-awesome-icon icon="search" id="searchIcon" />
+        <input placeholder="SEARCH (Client Name / Policy Number)" />
+      </nav>
+    </header>
     <List></List>
   </div>
 </template>
@@ -27,29 +19,4 @@ export default {
   },
 };
 </script>
-<style>
-.navbar {
-  background-color: #4d4768;
-  height: 80px;
-  display: flex;
-}
-#searchIcon {
-  color: darkgray;
-  margin-right: 20px;
-}
-input {
-  /* height: 100px; */
-  /* width: 100px; */
-  /* min-width: 100%; */
-  background-color: #4d4768;
-  padding: 0;
-  border-radius: 0;
-  transition: 0.2s;
-  font-size: 14rem;
-  color: #9b9b9b;
-  border: none;
-  outline: none;
-  border-bottom: 2px solid darkgray;
-
-}
-</style>
+<style src="../styles/search-bar.scss" lang="scss"></style>
